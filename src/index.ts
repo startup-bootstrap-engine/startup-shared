a30782443f09450e1680db5271742e202f811a46
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-06 18:36:30 -0800

    tiled types

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ export * from "./types/preloadScene.types";
 export * from "./types/resource.types";
 export * from "./types/scene.types";
 export * from "./types/socket.types";
+export * from "./types/tiled.types";
 export * from "./types/translation.types";
 export * from "./types/user.types";
 export * from "./types/validation.types";
