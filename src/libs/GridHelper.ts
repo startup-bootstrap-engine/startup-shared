b6e19fc904cebd015d18e24f64bbcefe9ce5117e
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-19 21:32:50 -0700

    togri from grid refactor

--- src/libs/GridHelper.ts
+++ src/libs/GridHelper.ts
@@ -1,7 +1,7 @@
 import { GRID_HEIGHT, GRID_WIDTH } from "../constants/world.constants";
 
-export const ToGridX = (x: number): number => Math.floor(x / GRID_WIDTH);
-export const ToGridY = (y: number): number => Math.floor(y / GRID_HEIGHT);
+export const ToGridX = (x: number): number => Math.round(x / GRID_WIDTH);
+export const ToGridY = (y: number): number => Math.round(y / GRID_HEIGHT);
 
-export const FromGridX = (gridX: number): number => gridX * GRID_WIDTH;
-export const FromGridY = (gridY: number): number => gridY * GRID_HEIGHT;
+export const FromGridX = (gridX: number): number => Math.round(gridX * GRID_WIDTH);
+export const FromGridY = (gridY: number): number => Math.round(gridY * GRID_HEIGHT);
