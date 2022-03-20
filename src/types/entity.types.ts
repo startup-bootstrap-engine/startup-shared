97173833de291b11095abf4494bcf13f9d6e3c49
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-20 15:34:06 -0700

    entity

--- src/types/entity.types.ts
+++ src/types/entity.types.ts
@@ -1,10 +1,10 @@
 export interface IEntityBasicInfo {
   id: string;
-  type: EntityType;
-  name: string;
-  x: number;
-  y: number;
-  direction: string;
+  type?: EntityType;
+  name?: string;
+  x?: number;
+  y?: number;
+  direction?: string;
 }
 
 export enum EntityType {
