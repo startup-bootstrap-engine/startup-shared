4e95736307112a042fbaad1f2162e5e5eacfb651
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-20 17:25:51 -0700

    emitToUser

--- src/types/socket.types.ts
+++ src/types/socket.types.ts
@@ -5,6 +5,6 @@ export enum SocketTypes {
 export interface ISocket {
   init: (socketType?: SocketTypes) => Promise<void> | void;
   onConnect?: (callback) => void;
-  emitToUser<T>(channel: string, eventName: string, data?: T);
+  emitToUser<T>(channel: string, eventName: string, data?: T, reliable?: boolean);
   emitToAllUsers<T>(eventName: string, data?: T): void;
 }
