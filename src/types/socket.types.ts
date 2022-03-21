303ae479a43ffad2fbb282b442da6e3c59d0aeac
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-20 17:29:15 -0700

    MovementStopped update

--- src/types/socket.types.ts
+++ src/types/socket.types.ts
@@ -5,6 +5,6 @@ export enum SocketTypes {
 export interface ISocket {
   init: (socketType?: SocketTypes) => Promise<void> | void;
   onConnect?: (callback) => void;
-  emitToUser<T>(channel: string, eventName: string, data?: T, reliable?: boolean);
+  emitToUser<T>(channel: string, eventName: string, data?: T);
   emitToAllUsers<T>(eventName: string, data?: T): void;
 }
