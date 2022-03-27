e958ce9c72f5c7bab671bbf9b53fe51378e0c562
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-27 16:52:27 -0700

    calculateNewXYPosition helper

--- src/libs/GridHelper.ts
+++ src/libs/GridHelper.ts
@@ -1,7 +1,42 @@
 import { GRID_HEIGHT, GRID_WIDTH } from "../constants/world.constants";
+import { AnimationDirection } from "../types/animation.types";
 
 export const ToGridX = (x: number): number => Math.round(x / GRID_WIDTH);
 export const ToGridY = (y: number): number => Math.round(y / GRID_HEIGHT);
 
 export const FromGridX = (gridX: number): number => Math.round(gridX * GRID_WIDTH);
 export const FromGridY = (gridY: number): number => Math.round(gridY * GRID_HEIGHT);
+
+interface INewCalculatedPosition {
+  x: number;
+  y: number;
+}
+
+export const calculateNewPositionXY = (
+  x: number,
+  y: number,
+  moveToDirection: AnimationDirection
+): INewCalculatedPosition => {
+  switch (moveToDirection) {
+    case "down":
+      return {
+        x,
+        y: y + GRID_HEIGHT,
+      };
+    case "up":
+      return {
+        x,
+        y: y - GRID_HEIGHT,
+      };
+    case "left":
+      return {
+        x: x - GRID_WIDTH,
+        y,
+      };
+    case "right":
+      return {
+        x: x + GRID_WIDTH,
+        y,
+      };
+  }
+};
