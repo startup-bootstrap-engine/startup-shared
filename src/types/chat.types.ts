42072b60952592b61f1331ec6e6d3767183b33c0
Guilherme Carvalho <guiazca@gmail.com>
2022-05-19 00:31:50 +0100

    add IResource, Typescript

--- src/types/chat.types.ts
+++ src/types/chat.types.ts
@@ -1,10 +1,10 @@
-import { IResource } from './resource.types';
+import { IResource } from "./resource.types";
 export enum ChatMessageType {
   Global = "Global",
   Private = "Private",
 }
 
-export interface IChatMessage extends IResource{
+export interface IChatMessage extends IResource {
   charId: string;
   message: string;
   type: ChatMessageType;
