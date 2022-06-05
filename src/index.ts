c941231f4c0e594e62c24e0b1cd4d723e84e2f0b
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2022-06-05 15:02:03 -0400

    changes

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,4 @@ export * from "./types/translation.types";
 export * from "./types/user.types";
 export * from "./types/validation.types";
 export * from "./types/view.types";
+
--- src/libs/MapZipHelper.ts
+++ src/libs/MapZipHelper.ts
@@ -1,42 +1,11 @@
-const { compress, decompress } = require("compress-json");
+const { compress } = require("compress-json");
 const fs = require("fs");
 const JSZip = require("jszip");
-let fetch = null;
-let file = null
-import("node-fetch").then(f => {  
-  fetch = f;
-});
-import("file-class").then(f => {
-  file = f;
-});
 
-export class MapZipHelper {
-  static async createZipMap(mapName: string, mapObject: object, pathToSave: string): Promise<void> {
-    const data = compress(mapObject);
-    const zip = new JSZip();
-    zip.file(`${mapName}.txt`, JSON.stringify(data));
-    const buffer = await zip.generateAsync({ type: "uint8array" });
-    await fs.createWriteStream(`${pathToSave}/${mapName}.zip`).write(buffer);
-  }
-
-  async readZip(fileName, filePath): Promise<object> {
-    const data = await (await fetch(filePath)).blob();
-
-    const file = new File([data], fileName, { type: "application/zip" });
-
-    const zipRead = new JSZip();
-    const content = await zipRead.loadAsync(file);
-
-    if (content.files && content.files[`${fileName}.txt`]) {
-      //@ts-ignore
-      const txtFile = content.files[`${fileName}.txt`]._data.compressedContent;
-      const bufferedString = Buffer.from(txtFile).toString();
-      const fileContent = decompress(JSON.parse(bufferedString));
-      return fileContent;
-    }
-
-    return {};
-  }
-}
-
-export const mapZipHelper = new MapZipHelper();
+export const createZipMap = async (mapName: string, mapObject: object, pathToSave: string): Promise<void> => {
+  const data = compress(mapObject);
+  const zip = new JSZip();
+  zip.file(`${mapName}.txt`, JSON.stringify(data));
+  const buffer = await zip.generateAsync({ type: "uint8array" });
+  await fs.createWriteStream(`${pathToSave}/${mapName}.zip`).write(buffer);
+};
