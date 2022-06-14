4ab6717627082f2865f47956d5350a29cbdb32cd
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-06-13 22:18:59 -0700

    improves skills types

--- src/types/skills.types.ts
+++ src/types/skills.types.ts
@@ -1,25 +1,56 @@
 import { IResource } from "./resource.types";
 
+interface ISkillDetails {
+  type: SkillType;
+  level: number;
+  skillPoints: number;
+  skillPointsToNextLevel?: number; //calculated dynamically
+  nextLevelRatio?: number; //calculated dynamically
+}
+
 export interface ISkill extends IResource {
   owner: string;
   level: number;
   xpGainRate: number;
   experience: number;
-  stamina: number;
-  magic: number;
-  strength: number;
-  resistance: number;
-  dexterity: number;
-  first: number;
-  club: number;
-  sword: number;
-  axe: number;
-  distance: number;
-  shielding: number;
-  fishing: number;
-  mining: number;
-  lumberjacking: number;
-  cooking: number;
-  alchemy: number;
-  magicResistance: number;
+  xpToNextLevel: number; // virtual field
+
+  // Basic attributes
+  stamina: ISkillDetails;
+  magic: ISkillDetails;
+  magicResistance: ISkillDetails;
+  strength: ISkillDetails;
+  resistance: ISkillDetails;
+  dexterity: ISkillDetails;
+
+  // Combat skills
+  first: ISkillDetails;
+  club: ISkillDetails;
+  sword: ISkillDetails;
+  axe: ISkillDetails;
+  distance: ISkillDetails;
+  shielding: ISkillDetails;
+
+  // Crafting skills
+  fishing: ISkillDetails;
+  mining: ISkillDetails;
+  lumberjacking: ISkillDetails;
+  cooking: ISkillDetails;
+  alchemy: ISkillDetails;
+
+  // Magic skills
+  ice: ISkillDetails;
+  fire: ISkillDetails;
+  earth: ISkillDetails;
+  air: ISkillDetails;
+  water: ISkillDetails;
+}
+
+export enum SkillType {
+  BasicAttributes = "BasicAttributes",
+  Combat = "Combat",
+  Crafting = "Crafting",
+  Gathering = "Gathering",
+  Magic = "Magic",
+  Misc = "Misc",
 }
