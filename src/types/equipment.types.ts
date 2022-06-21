9055947adce31bbb66d0cc2bc9b10ffb3b3d926b
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-06-20 19:33:11 -0700

    equipmentset inventory is an IItem

--- src/types/equipment.types.ts
+++ src/types/equipment.types.ts
@@ -1,18 +1,17 @@
 import { ICharacter } from "./character.types";
 import { IItem } from "./item.types";
-import { IItemContainer } from "./itemContainer.types";
 import { IResource } from "./resource.types";
 
 export interface IEquipementSet extends IResource {
-    owner?: ICharacter | string;
-    head?: IItem | string;
-    neck?: IItem | string;
-    leftHand?: IItem | string;
-    rightHand?: IItem | string;
-    ring?: IItem | string;
-    legs?: IItem | string;
-    boot?: IItem | string;
-    armor?: IItem | string;
-    acessory?: IItem | string;
-    inventory: IItemContainer | string;
+  owner?: ICharacter | string;
+  head?: IItem | string;
+  neck?: IItem | string;
+  leftHand?: IItem | string;
+  rightHand?: IItem | string;
+  ring?: IItem | string;
+  legs?: IItem | string;
+  boot?: IItem | string;
+  armor?: IItem | string;
+  acessory?: IItem | string;
+  inventory: IItem | string;
 }
