139fe285c2cf80e21d52fc39039fe8c731ce69c8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-06-30 22:09:36 -0700

    map types refactoring

--- src/types/maps.types.ts
+++ src/types/maps.types.ts
@@ -1,18 +1,21 @@
-export interface IMap {
-  JSONKey: string;
-  imageKey: string;
-  layers: string[];
-  tileWidth: number;
-  tileHeight: number;
-  imagePath: string;
-  jsonPath: string;
-  zipPath: string;
-  tilesetName: string;
-}
-
 export enum MapLayers {
   Ground = 0,
   OverGround = 1,
   Character = 2,
   OverCharacter = 3,
 }
+
+interface IMapMetadataTileset {
+  name: string;
+  imagePath: string;
+}
+
+export interface IMapMetaData {
+  hash: string;
+  layers: string[];
+  tileWidth: number;
+  tileHeight: number;
+  tilesets: IMapMetadataTileset[];
+}
+
+export const MAP_LAYERS = ["ground", "over-ground", "character", "over-character"];
