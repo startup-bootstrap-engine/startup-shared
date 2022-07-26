5f38b2d9270f631ed64978c85dcbca5ef9179079
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-07-25 23:24:47 -0700

    re-added decoration layer

--- src/types/maps.types.ts
+++ src/types/maps.types.ts
@@ -1,29 +1,32 @@
 export enum MapLayers {
   Ground = 0,
   OverGround = 1,
-  Character = 2,
-  OverCharacter = 3,
-  Roof = 4,
+  Decoration = 2,
+  Character = 3,
+  OverCharacter = 4,
+  Roof = 5,
 }
 
 export const TiledLayerNames = {
   0: "ground",
   1: "over-ground",
-  2: "character",
-  3: "over-character",
-  4: "roof",
+  2: "decoration",
+  3: "character",
+  4: "over-character",
+  5: "roof",
 };
 
 export const MAP_OBJECT_LAYERS = ["NPCs", "Transitions", "Items", "Roofs"];
 
-export const MAP_LAYERS = ["ground", "over-ground", "character", "over-character", "roof"];
+export const MAP_LAYERS = ["ground", "over-ground", "decoration", "character", "over-character", "roof"];
 
 export const MAP_LAYERS_TO_ID = {
   ground: 0,
   "over-ground": 1,
-  character: 2,
-  "over-character": 3,
-  roof: 4,
+  decoration: 2,
+  character: 3,
+  "over-character": 4,
+  roof: 5,
 };
 
 export const MAP_REQUIRED_LAYERS = [
@@ -34,6 +37,7 @@ export const MAP_REQUIRED_LAYERS = [
   "Transitions",
   "character",
   "Items",
+  "decoration",
   "over-ground",
   "ground",
 ];
