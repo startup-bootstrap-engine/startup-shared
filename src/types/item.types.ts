dbeeec19559876b58b1e49e18a53c8f8f11569d4
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2022-08-14 20:06:48 -0400

    fix conflicts

--- src/types/item.types.ts
+++ src/types/item.types.ts
@@ -172,7 +172,12 @@ export const ActionsByItemType = {
   ],
   CraftMaterial: [, ItemSocketEvents.GetItemInfo, ItemSocketEvents.ReadItemInfo, ItemSocketEvents.Drop],
   Other: [ItemSocketEvents.GetItemInfo, ItemSocketEvents.ReadItemInfo],
-  EquipmentSetItems: [ItemSocketEvents.Unequip, ItemSocketEvents.GetItemInfo, ItemSocketEvents.ReadItemInfo, ItemSocketEvents.Drop],
+  EquipmentSetItems: [
+    ItemSocketEvents.Unequip,
+    ItemSocketEvents.GetItemInfo,
+    ItemSocketEvents.ReadItemInfo,
+    ItemSocketEvents.Drop,
+  ],
   EquipmentSetContainer: [ItemSocketEvents.GetItemInfo, ItemSocketEvents.Unequip, ItemSocketEvents.ReadItemInfo],
 };
 
