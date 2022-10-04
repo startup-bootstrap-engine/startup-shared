f1e3709762a897afa48f605a39aa4a5a1c101dc5
Tom <tomasguerraalda@hotmail.com>
2022-10-04 17:55:38 -0300

    [RPG-821] UseWith System - add request interfaces and socket events

---
+++ src/types/useWith.types.ts
@@ -0,0 +1,23 @@
+
+/** REQUEST INTERFACES **/
+
+export interface IUseWithItem {
+    originItemId: string;
+    targetItemId: string;
+}
+
+export interface IUseWithTile {
+    originItemId: string;
+    targetTile: {
+        x: number;
+        y: number;
+        layer: string;
+    };
+}
+
+/** SOCKET EVENTS **/
+
+export enum UseWithSocketEvents {
+    UseWithItem = "UseWithItem",
+    UseWitTile = "UseWithTile"
+}
