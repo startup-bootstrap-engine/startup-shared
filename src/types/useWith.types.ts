e97a58140a048ed00b8727784e72b0082e3981d1
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-11-19 19:44:57 -0800

    use with types

--- src/types/useWith.types.ts
+++ src/types/useWith.types.ts
@@ -1,7 +1,41 @@
+import { ICharacter } from "./character.types";
 import { EntityType } from "./entity.types";
+import { IItem } from "./item.types";
 import { MapLayers } from "./maps.types";
 
-/** REQUEST INTERFACES **/
+export interface IUseWithTileEffect {
+  (item: IItem, targetTile: IUseWithTargetTile, character: ICharacter): Promise<void> | void;
+}
+
+export interface IUseWithItemEffect {
+  (targetItem: IItem, originItem: IItem, character: ICharacter): Promise<void> | void;
+}
+
+export interface IItemUseWithEntity extends IItem {
+  useWithItemEffect?: IUseWithItemEffect;
+  useWithTileEffect?: IUseWithTileEffect;
+}
+
+export interface IValidUseWithResponse {
+  originItem: IItem;
+  targetItem?: IItem;
+  useWithItemEffect?: IUseWithItemEffect;
+  useWithTileEffect?: IUseWithTileEffect;
+}
+
+export interface IMagicItemUseWithEntity extends IItem {
+  power: number;
+  animationKey: string;
+  projectileAnimationKey: string;
+  minMagicLevelRequired: number;
+}
+
+export interface IUseWithTargetTile {
+  x: number;
+  y: number;
+  map: string;
+  layer: MapLayers;
+}
 
 export interface IUseWithItem {
   originItemId: string;
@@ -10,12 +44,7 @@ export interface IUseWithItem {
 
 export interface IUseWithTile {
   originItemId: string;
-  targetTile: {
-    x: number;
-    y: number;
-    map: string;
-    layer: MapLayers;
-  };
+  targetTile: IUseWithTargetTile;
 }
 
 export interface IUseWithEntity {
