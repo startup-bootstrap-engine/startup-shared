b10278e3c736b3eb96609c3c0802329ec56f4ee6
Tom <tomasguerraalda@hotmail.com>
2022-12-22 21:21:34 -0300

    RPG-1184: add deposit/withdraw payload and events

--- src/types/depot.types.ts
+++ src/types/depot.types.ts
@@ -4,8 +4,22 @@ export interface IDepotContainerOpen {
   npcId: string;
 }
 
+export interface IDepotContainerWithdraw {
+  itemId: string;
+  npcId: string;
+  toContainerId: string;
+}
+
+export interface IDepotDepositItem {
+  itemId: string;
+  npcId: string;
+  fromContainerId?: string;
+}
+
 /** SOCKET EVENTS **/
 
 export enum DepotSocketEvents {
   OpenContainer = "OpenContainer",
+  Deposit = "Deposit",
+  Withdraw = "Withdraw",
 }
