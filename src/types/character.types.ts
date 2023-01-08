255aa56b5dbd50d8d9cc33d903c1bd2fcc449ea5
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-01-07 20:09:44 -0800

    position update from client

--- src/types/character.types.ts
+++ src/types/character.types.ts
@@ -155,6 +155,8 @@ export interface ICharacterCreateFromServer {
   textureKey: string;
 }
 export interface ICharacterPositionUpdateFromClient {
+  originX: number;
+  originY: number;
   newX: number;
   newY: number;
   direction: AnimationDirection;
