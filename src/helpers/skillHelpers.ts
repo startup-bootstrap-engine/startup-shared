fa3cb55370742728770ca3ff007dc15f36222ee9
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-01-14 22:20:53 -0800

    rollback

--- src/helpers/skillHelpers.ts
+++ src/helpers/skillHelpers.ts
@@ -1,5 +1,5 @@
 export const getSPForLevel = (level: number): number => {
-  const totalSPOnLevel = Math.pow(level, 3) * 2.5;
+  const totalSPOnLevel = Math.pow(level, 3) * 5;
   return totalSPOnLevel;
 };
 
