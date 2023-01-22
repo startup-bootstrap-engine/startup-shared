478d623d206f9c60359a122db006f318b5ce9077
Luiz Fischer <luiz-fischer@hotmail.com>
2023-01-21 21:05:01 -0300

    remove unused

--- src/types/skills.types.ts
+++ src/types/skills.types.ts
@@ -28,8 +28,6 @@ export interface ISkillDetails {
   level: number;
   skillPoints: number;
   skillPointsToNextLevel: number;
-  isBuffed?: boolean;
-  buffedLevel?: number;
 }
 
 export interface ISkill extends IResource {
