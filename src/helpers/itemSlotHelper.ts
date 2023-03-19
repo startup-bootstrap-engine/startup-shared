dd9ee186b577051cc1ea1c959b1ebb7bb046ed98
Marcelo Menegat <menegat.marcelo@gmail.com>
2023-03-19 18:19:39 +0000

    #RPG-1765: fix item sprite when is stackable and the quantity is below 1 like: 0.5, 0.2, ...

--- src/helpers/itemSlotHelper.ts
+++ src/helpers/itemSlotHelper.ts
@@ -2,20 +2,20 @@ export interface IItemTexturePathGetOptions {
   key: string;
   stackQty: number;
   texturePath: string;
-  isStackable: boolean
+  isStackable: boolean;
 }
 
 export const getItemTextureKeyPath = (requiredOptions: IItemTexturePathGetOptions, atlasJSON: any) => {
   const stackQty = requiredOptions?.stackQty ?? 0;
   const itemTexturePath = requiredOptions.texturePath;
 
-  if (requiredOptions && (stackQty >= 1 || (requiredOptions.isStackable && stackQty > 0 ))) {
+  if (requiredOptions && (stackQty >= 1 || (requiredOptions.isStackable && stackQty > 0))) {
     let idx = stackQty;
-    if( stackQty > 0 && stackQty < 1){
+    if (stackQty > 0 && stackQty < 1) {
       idx = 1;
-    } else if(stackQty >= 5) {
+    } else if (stackQty >= 5) {
       idx = 5;
-    } 
+    }
     const textureBreakPath = itemTexturePath.split(".");
     const txtPrefix: string = textureBreakPath[0];
     const txtExtension: string = textureBreakPath[1];
