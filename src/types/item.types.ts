324e9d5fbd44a5dcdd684373aeca83d65a17e817
Eduardo Carvalho <educarvalho009@gmail.com>
2023-05-10 20:38:23 -0400

    add: array buff at equippedBuff function

--- src/types/item.types.ts
+++ src/types/item.types.ts
@@ -333,7 +333,7 @@ export interface IEquippableItemBlueprint extends IBaseItemBlueprint {
   attack?: number;
   allowedEquipSlotType: ItemSlotType[];
   usableEffect?: (character: any) => void | Promise<void>;
-  equippedBuff?: ICharacterPermanentBuff;
+  equippedBuff?: ICharacterPermanentBuff | ICharacterPermanentBuff[];
 }
 
 export interface IEquippableRangedAmmoBlueprint extends IBaseItemBlueprint {
