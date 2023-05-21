4de3b870e75138b6b3c265e8ba42c17128c958f9
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-05-20 19:47:19 -0700

    exp gain

--- src/types/skills.types.ts
+++ src/types/skills.types.ts
@@ -5,6 +5,7 @@ import { IResource } from "./resource.types";
 export enum SkillSocketEvents {
   SkillGain = "SkillGain",
   ReadInfo = "ReadInfo",
+  ExperienceGain = "ExperienceGain",
 }
 
 export enum SkillEventType {
