6b3c60cd3bebd40f54b76402e09a5d455414c65c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-06-09 11:29:07 -0700

    socketio on disconnect

--- src/types/socket.types.ts
+++ src/types/socket.types.ts
@@ -5,6 +5,7 @@ export enum SocketTypes {
 export interface ISocket {
   init: (socketType?: SocketTypes) => Promise<void> | void;
   onConnect?: (callback) => void;
+  onDisconnect?: (callback) => void;
   emitToUser<T>(channel: string, eventName: string, data?: T);
   emitToAllUsers<T>(eventName: string, data?: T): void;
   disconnect: () => void;
