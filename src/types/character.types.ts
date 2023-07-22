1b1d2b78cf0f51eca276551e70a0896c28f97f93
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-07-21 22:20:15 -0700

    submit timestamp on position update

--- src/types/character.types.ts
+++ src/types/character.types.ts
@@ -165,7 +165,7 @@ export interface ICharacterPositionUpdateFromClient {
   newX: number;
   newY: number;
   direction: AnimationDirection;
-  timestamp?: Date;
+  timestamp?: number;
 }
 export interface ICharacterPositionUpdateFromServer {
   id: string; // will be validated server side
