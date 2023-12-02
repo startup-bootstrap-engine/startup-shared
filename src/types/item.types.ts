87a95a05b2da89bfb5f96236282e7347a91f75c6
Kalkidan <kal05627356@gmail.com>
2023-12-02 19:18:04 +0300

    isMageGear set to not be required

--- src/types/item.types.ts
+++ src/types/item.types.ts
@@ -336,7 +336,7 @@ export interface IEquippableItemBlueprint extends IBaseItemBlueprint {
   allowedEquipSlotType: ItemSlotType[];
   usableEffect?: (character: any) => void | Promise<void>;
   equippedBuff?: ICharacterPermanentBuff | ICharacterPermanentBuff[];
-  isMageGear: Boolean;
+  isMageGear?: Boolean;
 }
 
 export interface IEquippableRangedAmmoBlueprint extends IBaseItemBlueprint {
