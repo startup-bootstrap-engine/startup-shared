96af62926cb4a4e7419b2def92a79597b7424cc8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-12-12 21:26:37 -0800

    device detection bump

--- src/helpers/deviceHelpers.ts
+++ src/helpers/deviceHelpers.ts
@@ -7,14 +7,43 @@ const checkUserAgent = (regex: RegExp): boolean => {
   return regex.test(userAgent);
 };
 
+export const isMobile = (): boolean => {
+  const mobileRegex = /iPhone|Android.+Mobile/i;
+  return checkUserAgent(mobileRegex) && isTouchScreen();
+};
+
+export const isTablet = (): boolean => {
+  const tabletRegex = /iPad|Android/i;
+  return checkUserAgent(tabletRegex) && isTouchScreen();
+};
+
+export const isMobileOrTablet = (): boolean => {
+  return isMobile() || isTablet();
+};
+
+export const isDesktop = (): boolean => {
+  return !isMobileOrTablet();
+};
+
+/* !Evaluate this later
+
+// Helper function to check if the device is a touch screen
+const isTouchScreen = (): boolean => 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+// Helper function to check the user agent against a set of mobile/tablet identifiers
+const checkUserAgent = (regex: RegExp): boolean => {
+  const userAgent = navigator.userAgent || navigator.vendor;
+  return regex.test(userAgent);
+};
+
 // Check for device motion and orientation capabilities
 const hasMotionAndOrientation = (): boolean => {
-  return "DeviceMotionEvent" in window && "DeviceOrientationEvent" in window;
+  return 'DeviceMotionEvent' in window && 'DeviceOrientationEvent' in window;
 };
 
 // Estimate device performance capability
 const estimatePerformance = (): boolean => {
-  const isHighPerformance = window.matchMedia("(min-resolution: 2dppx)").matches;
+  const isHighPerformance = window.matchMedia('(min-resolution: 2dppx)').matches;
   return isHighPerformance;
 };
 
@@ -35,3 +64,5 @@ export const isMobileOrTablet = (): boolean => {
 export const isDesktop = (): boolean => {
   return !isMobileOrTablet() && !isTouchScreen();
 };
+  
+  */
