15e4daea1dda2769215232282ba5f6cfd23054b0
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-12-12 21:21:08 -0800

    device detection bump

--- src/helpers/deviceHelpers.ts
+++ src/helpers/deviceHelpers.ts
@@ -1,17 +1,37 @@
-import { isMobileOnly as detectIsMobileOnly, isMobile as detectIsMobileOrTablet } from "mobile-device-detect";
+// Helper function to check if the device is a touch screen
+const isTouchScreen = (): boolean => "ontouchstart" in window || navigator.maxTouchPoints > 0;
 
-const isSmallerThanBreakpoint = (breakpoint: string): boolean => {
-  const mql = window.matchMedia(
-    `(max-width: ${breakpoint}) and (orientation: portrait), (max-height: ${breakpoint}) and (orientation: landscape)`
-  );
-
-  return mql.matches;
+// Helper function to check the user agent against a set of mobile/tablet identifiers
+const checkUserAgent = (regex: RegExp): boolean => {
+  const userAgent = navigator.userAgent || navigator.vendor;
+  return regex.test(userAgent);
 };
 
-export const isMobileOrTablet = (): boolean => {
-  return detectIsMobileOrTablet ?? (isSmallerThanBreakpoint("767px") && !isSmallerThanBreakpoint("479px"));
+// Check for device motion and orientation capabilities
+const hasMotionAndOrientation = (): boolean => {
+  return "DeviceMotionEvent" in window && "DeviceOrientationEvent" in window;
+};
+
+// Estimate device performance capability
+const estimatePerformance = (): boolean => {
+  const isHighPerformance = window.matchMedia("(min-resolution: 2dppx)").matches;
+  return isHighPerformance;
 };
 
 export const isMobile = (): boolean => {
-  return detectIsMobileOnly ?? isSmallerThanBreakpoint("479px");
+  const mobileRegex = /iPhone|Android.+Mobile/i;
+  return checkUserAgent(mobileRegex) && isTouchScreen() && !estimatePerformance();
+};
+
+export const isTablet = (): boolean => {
+  const tabletRegex = /iPad|Android/i;
+  return checkUserAgent(tabletRegex) && isTouchScreen() && hasMotionAndOrientation();
+};
+
+export const isMobileOrTablet = (): boolean => {
+  return isMobile() || isTablet();
+};
+
+export const isDesktop = (): boolean => {
+  return !isMobileOrTablet() && !isTouchScreen();
 };
