d28cb998ded9af1df3aea8f21b7ecfd66c78b5e2
guiazca <guiazca@gmail.com>
2024-02-12 18:41:11 +0000

    adding types

---
+++ src/types/inGameStore.types.ts
@@ -0,0 +1,10 @@
+export enum InGameStoreItemBuyEvents {
+    BuyItem = "buyItem",
+    ReadItems = "readItems",
+  }
+  
+ export interface InGameStoreBuyPayload {
+    itemId: string;
+    coinType: "social-crystal" | "gold-coin";
+    price: number;
+  }
